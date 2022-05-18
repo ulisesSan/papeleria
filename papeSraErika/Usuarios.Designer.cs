40be90ea175f21d42a12efcff1697fdef37317e5
Ulises <uliseo_95@hotmail.com>
2022-05-18 16:28:13 -0500

    Arreglando bugs

--- papeSraErika/Usuarios.Designer.cs
+++ papeSraErika/Usuarios.Designer.cs
@@ -90,6 +90,7 @@ namespace papeSraErika
             this.txtPassword.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtPassword.Location = new System.Drawing.Point(162, 137);
             this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(161, 29);
             this.txtPassword.TabIndex = 3;
             // 
